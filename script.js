5c88d2cf66279b8ee20e22710eb35630e0a11875
Sviatoslav Semochko <semosvyatko@gmail.com>
2023-09-26 21:15:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Функція для закриття верхньої полоски
+function hideTopBar() {
+    const topBar = document.getElementById('top-bar');
+    topBar.style.height = '0'; // Змінюємо висоту на 0
+    topBar.style.padding = '0'; // Змінюємо padding на 0
+}
