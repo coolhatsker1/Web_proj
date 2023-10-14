b9ed062aa8c5f777f233efe8bf16288bc7e0dcbb
Sviatoslav Semochko <44783062+coolhatsker1@users.noreply.github.com>
2023-10-14 22:32:24 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Функція для закриття верхньої полоски
-function hideTopBar() {
-    const topBar = document.getElementById('top-bar');
-    topBar.style.height = '0'; // Змінюємо висоту на 0
-    topBar.style.padding = '0'; // Змінюємо padding на 0
-}
